b0b320f3c469781ac5d73fc41c65a0f3ef0cff5e
chrislo27 <kinchrislo@gmail.com>
2015-10-14 19:36:03 -0700

    Organize imports more better

--- core/src/chrislo27/spygame/screen/ScreenBase.java
+++ core/src/chrislo27/spygame/screen/ScreenBase.java
@@ -2,17 +2,16 @@ package chrislo27.spygame.screen;
 
 import com.badlogic.gdx.utils.Array;
 
+import chrislo27.spygame.Main;
 import ionium.screen.Updateable;
-import ionium.templates.Main;
-
 
 public class ScreenBase extends Updateable {
 
 	public chrislo27.spygame.Main main;
-	
-	public ScreenBase(chrislo27.spygame.Main m) {
+
+	public ScreenBase(Main m) {
 		super(m);
-		
+
 		this.main = m;
 	}
 
