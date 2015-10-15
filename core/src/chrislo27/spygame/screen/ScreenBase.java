706db000294762eee38c10a53ea7f9253c30fc00
chrislo27 <kinchrislo@gmail.com>
2015-10-14 19:36:28 -0700

    Oops ScreenBase was supposed to be abstract

--- core/src/chrislo27/spygame/screen/ScreenBase.java
+++ core/src/chrislo27/spygame/screen/ScreenBase.java
@@ -5,7 +5,7 @@ import com.badlogic.gdx.utils.Array;
 import chrislo27.spygame.Main;
 import ionium.screen.Updateable;
 
-public class ScreenBase extends Updateable {
+public abstract class ScreenBase extends Updateable {
 
 	public chrislo27.spygame.Main main;
 
@@ -13,47 +13,6 @@ public class ScreenBase extends Updateable {
 		super(m);
 
 		this.main = m;
-	}
-
-	@Override
-	public void render(float delta) {
-	}
-
-	@Override
-	public void renderUpdate() {
-	}
-
-	@Override
-	public void tickUpdate() {
-	}
-
-	@Override
-	public Array<String> getDebugStrings(Array<String> array) {
-		return null;
-	}
-
-	@Override
-	public void resize(int width, int height) {
-	}
-
-	@Override
-	public void show() {
-	}
-
-	@Override
-	public void hide() {
-	}
-
-	@Override
-	public void pause() {
-	}
-
-	@Override
-	public void resume() {
-	}
-
-	@Override
-	public void dispose() {
 	}
 
 }
