2c94b98097cebecbe7d7f7c9effec52ebe23e968
chrislo27 <kinchrislo@gmail.com>
2015-10-18 11:20:11 -0700

    No more french

--- core/src/chrislo27/spygame/load/LanguageAdder.java
+++ core/src/chrislo27/spygame/load/LanguageAdder.java
@@ -10,8 +10,8 @@ import ionium.util.i18n.NamedLocale;
 public class LanguageAdder {
 
 	public static final void addLanguages() {
-		Localization.instance().addBundle(
-				new NamedLocale("French", new Locale("fr"), "flag_france", "flag_quebec"));
+//		Localization.instance().addBundle(
+//				new NamedLocale("French", new Locale("fr"), "flag_france", "flag_quebec"));
 
 		LanguageScreen langScreen = ScreenRegistry.get("langSelect", LanguageScreen.class);
 		if (langScreen != null) {
