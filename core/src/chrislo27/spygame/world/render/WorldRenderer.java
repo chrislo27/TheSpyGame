adacbb43ab628f73872fc17147c17a446413b8ad
chrislo27 <kinchrislo@gmail.com>
2015-10-31 11:17:59 -0700

    Added camera to world renderer

--- core/src/chrislo27/spygame/world/render/WorldRenderer.java
+++ core/src/chrislo27/spygame/world/render/WorldRenderer.java
@@ -1,20 +1,27 @@
 package chrislo27.spygame.world.render;
 
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 import chrislo27.spygame.Main;
 import chrislo27.spygame.world.World;
+import ionium.registry.GlobalVariables;
 
 public class WorldRenderer {
 
 	public World world;
 	public SpriteBatch batch;
 	public Main main;
-	
-	public WorldRenderer(World world, Main main){
+	public OrthographicCamera camera;
+
+	public WorldRenderer(World world, Main main) {
 		this.world = world;
 		this.main = main;
 		this.batch = main.batch;
+
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, GlobalVariables.getInt("DEFAULT_WIDTH") * 1f / World.PX_UNIT,
+				GlobalVariables.getInt("DEFAULT_HEIGHT") * 1f / World.PX_UNIT);
 	}
-	
+
 }
