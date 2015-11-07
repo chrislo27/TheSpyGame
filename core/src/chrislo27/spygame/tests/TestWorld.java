c3b1fef83d7fcf5dd0d28f8821c0f9f79d322758
chrislo27 <kinchrislo@gmail.com>
2015-11-06 18:09:46 -0800

    Removed other players

--- core/src/chrislo27/spygame/tests/TestWorld.java
+++ core/src/chrislo27/spygame/tests/TestWorld.java
@@ -8,15 +8,7 @@ public class TestWorld extends World{
 	public TestWorld(float sizex, float sizey) {
 		super(sizex, sizey);
 		
-		entities.add(new EntityPlayer(this, 0, 10));
-		entities.add(new EntityPlayer(this, 0, 8));
-		entities.add(new EntityPlayer(this, 0, 6));
-		entities.add(new EntityPlayer(this, 0, 4));
-		
 		entities.add(new EntityPlayer(this, 4, 10));
-		entities.add(new EntityPlayer(this, 4, 8));
-		entities.add(new EntityPlayer(this, 2, 6));
-		entities.add(new EntityPlayer(this, 8, 4));
 	}
 
 }
