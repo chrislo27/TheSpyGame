2c79e2c60d5fb90c87a3dee79ac908c5e300edf9
chrislo27 <kinchrislo@gmail.com>
2015-11-07 15:36:54 -0800

    Test input

--- core/src/chrislo27/spygame/world/World.java
+++ core/src/chrislo27/spygame/world/World.java
@@ -1,5 +1,8 @@
 package chrislo27.spygame.world;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.utils.Array;
 
 import chrislo27.spygame.entity.Entity;
@@ -10,7 +13,7 @@ public class World {
 	public static final float UNIT_PX = (1f / PX_UNIT);
 
 	public static final float FLOOR = 0f;
-	public static final float DRAG = 5f;
+	public static final float DRAG = 10f;
 
 	public float gravity = 1.5f;
 
@@ -42,7 +45,19 @@ public class World {
 	}
 
 	public void inputUpdate() {
+		if (Gdx.input.isKeyPressed(Keys.LEFT)) {
+			entities.get(0).veloX = MathUtils.clamp(entities.get(0).veloX
+					- (Gdx.graphics.getDeltaTime() * 50f) - (Gdx.graphics.getDeltaTime() * DRAG),
+					-5f, 5f);
+		} else if (Gdx.input.isKeyPressed(Keys.RIGHT)) {
+			entities.get(0).veloX = MathUtils.clamp(entities.get(0).veloX
+					+ (Gdx.graphics.getDeltaTime() * 50f) + (Gdx.graphics.getDeltaTime() * DRAG),
+					-5f, 5f);
+		}
 
+		if (Gdx.input.isKeyJustPressed(Keys.UP)) {
+			entities.get(0).veloY = 15f;
+		}
 	}
 
 }
