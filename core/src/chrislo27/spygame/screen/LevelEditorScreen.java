833eaf0d2f8020f0d9469561e30cc39f85b30906
chrislo27 <kinchrislo@gmail.com>
2015-11-11 12:16:56 -0800

    Basic stuff for rendering world

--- core/src/chrislo27/spygame/screen/LevelEditorScreen.java
+++ core/src/chrislo27/spygame/screen/LevelEditorScreen.java
@@ -3,20 +3,29 @@ package chrislo27.spygame.screen;
 import com.badlogic.gdx.utils.Array;
 
 import chrislo27.spygame.Main;
+import chrislo27.spygame.world.World;
+import chrislo27.spygame.world.render.WorldRenderer;
 import ionium.screen.Updateable;
 
 public class LevelEditorScreen extends Updateable<Main>{
 
+	World world;
+	WorldRenderer renderer;
+	
 	public LevelEditorScreen(Main m) {
 		super(m);
+		
+		renderer = new WorldRenderer(null, main);
 	}
 
 	@Override
 	public void render(float delta) {
+		renderer.render();
 	}
 
 	@Override
 	public void renderUpdate() {
+		renderer.renderUpdate();
 	}
 
 	@Override
